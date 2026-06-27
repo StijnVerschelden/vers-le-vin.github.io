3781956b29cedb31348a15896e39f15246301c76
SetarehRooz <sf.roozbahani@gmail.com>
2026-06-27 13:32:59 +0200

    modified:   index.js

--- index.js
+++ index.js
@@ -197,9 +197,9 @@ document.addEventListener("DOMContentLoaded", () => {
     { img: "images/beelden/natuurenik.jpg",   keyword: "Nature" },
     { img: "images/beelden/photography1.jpg", keyword: "Analog Photography"},
     { img: "images/beelden/filmmaker2.jpg", keyword: "Filmmaker"},
-    { img: "images/beelden/photography2.jpg", keyword: "Photography"},
     {img: "images/beelden/filmposter.png", keyword: "Short Film"},
-  ];
+    { img: "images/beelden/photography2.jpg", keyword: "Travelogue"},
+    
 
   const SECONDS_PER_SLIDE = 3.5;
   const FLIP_DURATION_MS = 1400;
